5a8eb67315bdcdbe5511c1d8cf06ed49d40c29bb
Russ Garrett <russ@garrett.co.uk>
2020-10-04 15:17:22 +0100

    Improve front page

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ create schema solartag;
 
 create materialized view solartag.candidates AS SELECT osm_id, geometry from osm_power_generator, countries.countries where st_intersects(st_transform(countries.wkb_geometry, 3857), geometry) and countries.iso = 'GBR' and ST_GeometryType(geometry) = 'ST_Point' and source = 'solar' and output = '' and tags->'generator:solar:modules' is null;
 
-create index solartag_candidates_osm_id on solartag.candidates(osm_id);
+create unique index solartag_candidates_osm_id on solartag.candidates(osm_id);
 
 CREATE TABLE solartag.users (id INTEGER PRIMARY KEY,
 				display_name TEXT NOT NULL,
